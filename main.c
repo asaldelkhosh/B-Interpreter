226fde48513cfa068480c9130504319806c06cff
amirhnajafiz <najafizadeh21@gmail.com>
2023-11-09 12:36:29 +0330

    add: operator method check

--- main.c
+++ main.c
@@ -5,9 +5,28 @@
 #include <stdbool.h>
 
 
+// constant parameters
+#define OPERATIONS "+-*/%="
+
+
 // check if character is alphabet or not
 bool is_alphabet(char ch) {
     return isalpha(ch);
+}
+
+// check if character is digit or not
+bool is_digital(char ch) {
+    return isdigit(ch);
+}
+
+// check if character is mathematical operation
+bool is_operator(char ch) {
+    for (int i = 0; i < sizeof(OPERATIONS); i++) {
+        if (ch == OPERATIONS[i])
+            return true;
+    }
+
+    return false;
 }
 
 
@@ -30,7 +49,7 @@ int main()
         if (ch =='\n' || ch == '\0' || ch == EOF)
             continue;
 
-        printf("%c -> %d\n", ch, is_alphabet(ch));
+        printf("%c -> alp:%d , num:%d , opr:%d\n", ch, is_alphabet(ch), is_digital(ch), is_operator(ch));
     }
 
 	// closing the file
